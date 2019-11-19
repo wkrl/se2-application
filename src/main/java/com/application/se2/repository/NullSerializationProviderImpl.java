da562f1567de855b9b780981409430d62750a8eb
Sven Graupner <sgraupner@beuth-hochschule.de>
2019-11-19 21:35:40 +0100

    Initial version for Entity persistence using Java serialization.

---
+++ src/main/java/com/application/se2/repository/NullSerializationProviderImpl.java
@@ -0,0 +1,48 @@
+package com.application.se2.repository;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.application.se2.model.Entity;
+
+
+class NullSerializationProviderImpl implements SerializationProviderIntf {
+
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param path path to JSON file.
+	 * @param clazz entity class needed for de-serialization.
+	 */
+	NullSerializationProviderImpl( String path, Class<? extends Entity> clazz ) {
+
+	}
+
+
+	/**
+	 * Serialize list of entities into serial JSON stream. The full object
+	 * tree is serialized.
+	 * 
+	 * @param list list of entities to serialize.
+	 * @exception throws IOException.
+	 */
+	@Override
+	public void writeSerialStream( List<? extends Entity> list ) throws IOException {
+
+	}
+
+
+	/**
+	 * Deserialize JSON-stream into entities. The collector interface is called
+	 * when an entity was deserialized passing the entity to the invoking method.
+	 * 
+	 * @param collector functional interface to invoke caller passing a deserialized entity.
+	 * @exception throws IOException.
+	 */
+	@Override
+	public void readSerialStream( CollectorIntf collector ) throws IOException {
+
+	}
+
+}
